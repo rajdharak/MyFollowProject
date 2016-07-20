d2098d593ea65f6aa1bdcde4e08f292e4ed98007
rajdharak <raj@promact.com>
2016-07-20 12:45:33 +0530

    Add FollowContext Model

---
+++ Myfollow/Myfollow/Models/FollowContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Myfollow.Models
+{
+    public class FollowContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<AddressInfo> AddressInfo { get; set; }
+        public DbSet<CommonProperties> CommonProprtties { get; set; }
+        public DbSet<Media> Media { get; set; }
+        public DbSet<UpdateDetail> UpdateDetails { get; set; }
+        public DbSet<Followers> Followers { get; set; }
+    }
+}
