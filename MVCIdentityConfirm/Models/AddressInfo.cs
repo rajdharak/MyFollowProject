65f95552e044bb1bfb99d37d266f26ed2f330451
rajdharak <raj@promact.com>
2016-07-23 11:41:27 +0530

    Identity Confirmation is done

---
+++ MVCIdentityConfirm/Models/AddressInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCIdentityConfirm.Models
+{
+    public class AddressInfo
+    {
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+    }
+}
