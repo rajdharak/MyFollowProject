abc61777df1e23b2c12b1217ad8ca5de3c331a8d
rajdharak <raj@promact.com>
2016-07-26 10:56:50 +0530

    Add project files.

---
+++ MyFollow/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFollow.Startup))]
+namespace MyFollow
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
