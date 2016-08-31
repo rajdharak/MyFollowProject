f7b20fc2f24ac7fe3145bd9b51b308c16652d4de
rajdharak <raj@promact.com>
2016-08-31 17:02:51 +0530

    31-08-2016

--- MyFollowOwin/Controllers/HomeController.cs
+++ MyFollowOwin/Controllers/HomeController.cs
@@ -6,9 +6,10 @@ using System.Web.Mvc;
 
 namespace MyFollowOwin.Controllers
 {
-    [Authorize]
+    
     public class HomeController : Controller
-    {        
+    {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
